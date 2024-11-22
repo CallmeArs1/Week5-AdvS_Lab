27f0c6a8dd0f9077218f325d0c92b48d03205906
CodeSandbox <support@codesandbox.io>
2024-11-22 22:15:59 +0000

    Sandbox snapshot commit

--- src/app.js
+++ src/app.js
@@ -1,24 +1,25 @@
-import fetchData from './app-internals/fetchData';
-import form from './app-internals/form';
+import fetchData from "./app-internals/fetchData";
+import form from "./app-internals/form";
 
 // web components' import
-import './components/hello-world';
-import './components/demo-component';
-import './components/model-chooser';
-import './components/model-viewer';
+import "./components/hello-world";
+import "./components/demo-component";
+import "./components/model-chooser";
+import "./components/model-viewer";
+import "./lit-components/lit-hello-world";
 
-const appElement = document.getElementById('app');
+const appElement = document.getElementById("app");
 
 appElement.innerHTML = `
   <hello-world></hello-world>
   <hello-world name="Web Components"></hello-world>
   
   <demo-component 
-    title="Demo Component"
-    image-src="https://cdn.worldvectorlogo.com/logos/mercedes-benz.svg"
-    >
+    title="My Title"
+    image-src="http://www.w3.org/2000/svg"
     <p class="notes" slot="notes">This is a demo component</p>
   </demo-component>
+  <lit-hello-world name="Arselan Khan"></lit-hello-world> <!-- Added the lit-hello-world component -->
 
   ${form}
   
@@ -32,34 +33,34 @@ appElement.innerHTML = `
   </div>
 `;
 
-const chooser = appElement.querySelector('model-chooser');
-const viewer = appElement.querySelector('model-viewer');
-const showDetailsInput = appElement.querySelector('[name=show-details]');
-const urlInput = appElement.querySelector('[name=url]');
-const limitInput = appElement.querySelector('[name=limit]');
+const chooser = appElement.querySelector("model-chooser");
+const viewer = appElement.querySelector("model-viewer");
+const showDetailsInput = appElement.querySelector("[name=show-details]");
+const urlInput = appElement.querySelector("[name=url]");
+const limitInput = appElement.querySelector("[name=limit]");
 
 const loadList = () =>
-  fetchData(urlInput.value).then(data => (chooser.items = data));
+  fetchData(urlInput.value).then((data) => (chooser.items = data));
 
-appElement.addEventListener('model-change', event => {
+appElement.addEventListener("model-change", (event) => {
   if (!viewer) return;
   viewer.data = chooser.items[event.detail];
 });
 
-showDetailsInput.addEventListener('change', event => {
+showDetailsInput.addEventListener("change", (event) => {
   const enabled = !!showDetailsInput.value;
   if (enabled) {
-    viewer.setAttribute('show-details', '');
+    viewer.setAttribute("show-details", "");
   } else {
-    viewer.removeAttribute('show-details');
+    viewer.removeAttribute("show-details");
   }
 });
-urlInput.addEventListener('change', event => {
+urlInput.addEventListener("change", (event) => {
   loadList();
 });
 loadList();
 
-limitInput.addEventListener('change', event => {
+limitInput.addEventListener("change", (event) => {
   // chooser.setAttribute('limit', event.currentTarget.value)
   chooser.limit = event.currentTarget.value;
 });
